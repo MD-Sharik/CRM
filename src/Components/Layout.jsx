820007a82c84d5a1c2f87a7c985c408bb0ccf6bf
MD Sharik <md.sharik5012@gmail.com>
2024-06-05 12:11:10 +0530

    asd

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -31,10 +31,10 @@ function Layout() {
   // Check if the current path is in the dashboardPaths array
   const isDashboard = dashboardPaths.includes(location.pathname);
 
-  // Additional debugging information
-  console.log("Current path:", location.pathname); // For debugging
-  console.log("Dashboard paths:", dashboardPaths); // For debugging
-  console.log("Is dashboard:", isDashboard); // For debugging
+  // // Additional debugging information
+  // console.log("Current path:", location.pathname); // For debugging
+  // console.log("Dashboard paths:", dashboardPaths); // For debugging
+  // console.log("Is dashboard:", isDashboard); // For debugging
 
   return (
     <div>
