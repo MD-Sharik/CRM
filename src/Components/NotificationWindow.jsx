bfc7d88b13ec3985ff32e0691c7f0271c5790395
MD Sharik <md.sharik5012@gmail.com>
2024-06-26 18:07:50 +0530

    changes to url

--- src/Components/NotificationWindow.jsx
+++ src/Components/NotificationWindow.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function NotificationWindow() {
   return (
-    <div className="absolute right-0 top-10 z-10 p-2 h-[20rem] w-[18rem] sm:min-w-[28rem] text-black font-bold rounded-lg shadow-lg  bg-yellow-200 ">
+    <div className="absolute  -right-16 xl:-right-0 top-10 z-10 p-2 h-[20rem] w-[18rem] sm:min-w-[16rem] text-black font-bold rounded-lg shadow-lg  bg-yellow-200 ">
       Notification Center
       <hr className="my-3 border-black" />
     </div>
